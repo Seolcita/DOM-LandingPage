3cc045d6b4f849ac6f7f1bb7f207fbce74ca9886
Seolcita <seolcita@gmail.com>
2021-10-28 11:26:03 -0600

    Nav hover effect

--- script.js
+++ script.js
@@ -12,6 +12,25 @@ const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
 
 ///////////////////////////////////////
+// Navigation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+///////////////////////////////////////
 // Modal window
 
 const openModal = function (e) {
@@ -57,4 +76,36 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
+});
+
+///////////////////////////////////////
+// Tabs
+
+// This is not practical, if there are 2000 tabs,
+// callback function will be created 2000 times
+// It will slow down page
+//tabs.forEach(t => t.addEventListener('click', () => console.log('Clicked')));
+
+tabsContainer.addEventListener('click', e => {
+  // e.target.closest >> Find the target's closest parent
+  const clicked = e.target.closest('.operations__tab');
+  //console.log(clicked);
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Active Tab
+  // Remove lifted button before lift just clicked one
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  // Lift just clicked one
+  clicked.classList.add('operations__tab--active');
+
+  // Active Content Area
+  // Remove lifted tab content before display just selected one
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  // Display just selected content
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+  // console.log(clicked.dataset.tab);
 });
